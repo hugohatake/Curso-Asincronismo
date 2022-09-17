73049f68a274a3afc3cf0e426de4d15f9157abb5
hatake <hugoro3000@gmail.com>
2022-09-16 22:34:38 -0500

    generators para Api

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -2,8 +2,8 @@ import fetch from 'node-fetch';
 const API = 'https://api.escuelajs.co/api/v1'; 
 
 async function fetchData(urlApi) {
-    const response = fetch(urlApi);
-    const data = await response.json();
+    const response = await fetch(urlApi);
+    const data = await response.json(); 
     return data;
 }
 
